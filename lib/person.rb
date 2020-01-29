d885c363b499dae232c5cb468e78eac3d98cf39e
Asha Fernandez <ash.fernan90@gmail.com>
2020-01-29 01:34:02 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width,
+
+  def initialize (attributes=nil)
+    if attributes
+      attributes.each do |a,f|
+        self.send("#{a}=", f)
+      end
+    end
+  end
 end
